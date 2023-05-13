766721af73ba339c96d2c94763a49c0b382da968
Prithwish Samanta <wprith@gmail.com>
2023-05-13 22:28:12 +0530

    implement open api doc for movie-catalog-service

--- Backend/movie-catalog-service/src/main/java/com/cts/moviecatalogservice/controller/MovieCatalogController.java
+++ Backend/movie-catalog-service/src/main/java/com/cts/moviecatalogservice/controller/MovieCatalogController.java
@@ -7,22 +7,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cts.moviecatalogservice.dto.AllMovieResponse;
+import com.cts.moviecatalogservice.dto.ErrorResponse;
 import com.cts.moviecatalogservice.dto.MovieDto;
 import com.cts.moviecatalogservice.service.MovieCatalogService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Movie Catalog", description = "movie API")
 @RestController
 public class MovieCatalogController {
 	@Autowired
 	private MovieCatalogService catalogService;
 
+	@Operation(summary = "View all movies", description = "Get list of currently available movies in theaters")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "All movie retrieve successful", content = @Content(schema = @Schema(implementation = AllMovieResponse.class))) })
 	@GetMapping("/all")
 	public ResponseEntity<AllMovieResponse> getAllMovies() {
 		AllMovieResponse response = catalogService.getAllMovies();
 		return ResponseEntity.ok(response);
 	}
 
+	@Operation(summary = "Search movie by id", description = "Get specific movie details by its id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Movie details retrieve successful", content = @Content(schema = @Schema(implementation = MovieDto.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))) })
 	@GetMapping("/movies/search/{movieById}")
-	public ResponseEntity<MovieDto> getMovieById(@PathVariable String movieById) {
+	public ResponseEntity<MovieDto> getMovieById(
+			@Parameter(description = "Id of the movie", required = true) @PathVariable String movieById) {
 		MovieDto movie = catalogService.getMovieById(movieById);
 		return ResponseEntity.ok(movie);
 	}
