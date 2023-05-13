bcd679d60201f17d0f9d9dc4bdf3e1182e010fd6
Prithwish Samanta <wprith@gmail.com>
2023-05-13 16:45:05 +0530

    implement open api doc for user-auth-service

--- Backend/user-auth-service/src/main/java/com/cts/userauthservice/controller/AuthController.java
+++ Backend/user-auth-service/src/main/java/com/cts/userauthservice/controller/AuthController.java
@@ -26,13 +26,27 @@ import com.cts.userauthservice.dto.Response;
 import com.cts.userauthservice.dto.ValidationDto;
 import com.cts.userauthservice.service.AuthService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Authentication", description = "the user auth API")
 @RestController
 public class AuthController {
 	@Autowired
 	private AuthService authService;
 
+	@Operation(summary = "Register new user", description = "Create a new user account with provided credentials.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Successful registration", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "409", description = "Email already exists", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@PostMapping("/register")
-	public ResponseEntity<Response> register(@RequestBody @Valid RegistrationRequest registrationRequest,
+	public ResponseEntity<Response> register(
+			@Parameter(description = "Registration request containing firstname, lastname, email, password and secret-question", required = true) @RequestBody @Valid RegistrationRequest registrationRequest,
 			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return errorResponse(bindingResult.getAllErrors());
@@ -43,8 +57,14 @@ public class AuthController {
 				HttpStatus.CREATED);
 	}
 
+	@Operation(summary = "Login user", description = "Authenticate user and generate a JWT token for accessing protected resources.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Successful login", content = @Content(schema = @Schema(implementation = LoginResponse.class))),
+			@ApiResponse(responseCode = "400", description = "Bad credentials", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody @Valid LoginRequest loginRequest, BindingResult bindingResult) {
+	public ResponseEntity<?> login(
+			@Parameter(description = "Login request containing email and password", required = true) @RequestBody @Valid LoginRequest loginRequest,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return errorResponse(bindingResult.getAllErrors());
 		}
@@ -52,9 +72,15 @@ public class AuthController {
 		return new ResponseEntity<LoginResponse>(response, HttpStatus.OK);
 	}
 
+	@Operation(summary = "Forgot Password", description = "Reset user password after checking the answer to the security question.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Password reset successful", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@GetMapping("/{userid}/forgot")
-	public ResponseEntity<Response> forgotPassword(@PathVariable String userid,
-			@RequestBody @Valid PasswordChangeRequest passwordChangeRequest, BindingResult bindingResult) {
+	public ResponseEntity<Response> forgotPassword(
+			@Parameter(description = "Id of the user", required = true) @PathVariable String userid,
+			@Parameter(description = "Forgot password request containing new password, security question and answer") @RequestBody @Valid PasswordChangeRequest passwordChangeRequest,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return errorResponse(bindingResult.getAllErrors());
 		}
@@ -64,9 +90,15 @@ public class AuthController {
 				HttpStatus.OK);
 	}
 
+	@Operation(summary = "Update Password", description = "Update the current password of the logged in user.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Password update successful", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@PutMapping("/{userid}/updatepassword")
-	public ResponseEntity<Response> updatePassword(@PathVariable String userid,
-			@RequestBody @Valid PasswordChangeRequest passwordChangeRequest, BindingResult bindingResult) {
+	public ResponseEntity<Response> updatePassword(
+			@Parameter(description = "Id of the user", required = true) @PathVariable String userid,
+			@Parameter(description = "Update password request containing new password, security question and answer") @RequestBody @Valid PasswordChangeRequest passwordChangeRequest,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return errorResponse(bindingResult.getAllErrors());
 		}
@@ -76,8 +108,14 @@ public class AuthController {
 				HttpStatus.OK);
 	}
 
+	@Operation(summary = "Validate JWT Token", description = "Validate the provided JWT token and check if it is still valid or expired.")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Valid JWT token", content = @Content(schema = @Schema(implementation = ValidationDto.class))),
+			@ApiResponse(responseCode = "401", description = "Invalid token", content = @Content(schema = @Schema(implementation = ValidationDto.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@GetMapping("/validate")
-	public ResponseEntity<ValidationDto> validateAuthToken(@RequestHeader(name = "Authorization") String jwtToken) {
+	public ResponseEntity<ValidationDto> validateAuthToken(
+			@Parameter(description = "JWT token to validate", required = true) @RequestHeader(name = "Authorization") String jwtToken) {
 		ValidationDto response = authService.validateAuthToken(jwtToken);
 		if (response.isStatus())
 			return ResponseEntity.ok(authService.validateAuthToken(jwtToken));
