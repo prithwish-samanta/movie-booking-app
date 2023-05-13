b6db6e76497953d4c35e10cbcee106883ec090cc
Prithwish Samanta <wprith@gmail.com>
2023-05-13 23:24:02 +0530

    implement open api doc for admin-service

---
+++ Backend/admin-service/src/main/java/com/cts/adminservice/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.cts.adminservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+public class OpenApiConfig {
+	@Bean
+	OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption,
+			@Value("${application-version}") String appVersion) {
+		return new OpenAPI().info(new Info().title("Admin Service").version(appVersion)
+				.description(appDesciption).contact(new Contact().name("Prithwish Samanta").email("wprith@gmail.com"))
+				.license(new License().name("MIT")));
+	}
+}
--- Backend/admin-service/src/main/java/com/cts/adminservice/controller/AdminController.java
+++ Backend/admin-service/src/main/java/com/cts/adminservice/controller/AdminController.java
@@ -22,21 +22,46 @@ import com.cts.adminservice.dto.AddMovieRequest;
 import com.cts.adminservice.dto.Response;
 import com.cts.adminservice.service.AdminService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Admin", description = "admin API")
 @RestController
 public class AdminController {
 	@Autowired
 	private AdminService adminService;
 
+	@Operation(summary = "Update ticket status", description = "Update an existing ticket status")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Ticket updation successful", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "404", description = "Ticket not found", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorize. Only a admin can perform this operation", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@PutMapping("/update/{ticketId}/{newStatus}")
-	public ResponseEntity<Response> updateTicketStatus(@RequestHeader(name = "Authorization") String token,
-			@PathVariable String ticketId, @PathVariable String newStatus) {
+	public ResponseEntity<Response> updateTicketStatus(
+			@Parameter(description = "JWT auth token of the application user", required = true) @RequestHeader(name = "Authorization") String token,
+			@Parameter(description = "Id of the ticket", required = true) @PathVariable String ticketId,
+			@Parameter(description = "New updated ticket status", required = true) @PathVariable String newStatus) {
 		adminService.updateTicketStatus(token, ticketId, newStatus);
 		return ResponseEntity.ok(Response.builder().status("success").message("Ticket updated successfully").build());
 	}
 
+	@Operation(summary = "Add movie", description = "Add a new movie")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "New movie addition successful", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "404", description = "Not found", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorize. Only a admin can perform this operation", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@PostMapping("/addmovie")
-	public ResponseEntity<Response> addMovie(@RequestHeader(name = "Authorization") String token,
-			@RequestBody @Valid AddMovieRequest addMovieRequest, BindingResult bindingResult) {
+	public ResponseEntity<Response> addMovie(
+			@Parameter(description = "JWT auth token of the application user", required = true) @RequestHeader(name = "Authorization") String token,
+			@Parameter(description = "Add movie request with all details", required = true) @RequestBody @Valid AddMovieRequest addMovieRequest,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return errorResponse(bindingResult.getAllErrors());
 		}
@@ -45,9 +70,16 @@ public class AdminController {
 				HttpStatus.CREATED);
 	}
 
+	@Operation(summary = "Delete movie", description = "Delete an existing movie")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Movie deletion successful", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "404", description = "Movie not found", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorize. Only a admin can perform this operation", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@DeleteMapping("/delete/{movieId}")
-	public ResponseEntity<Response> deleteMovie(@RequestHeader(name = "Authorization") String token,
-			@PathVariable String movieId) {
+	public ResponseEntity<Response> deleteMovie(
+			@Parameter(description = "JWT auth token of the application user", required = true) @RequestHeader(name = "Authorization") String token,
+			@Parameter(description = "Id of the movie to be deleted", required = true) @PathVariable String movieId) {
 		adminService.deleteMovie(token, movieId);
 		return ResponseEntity.ok(Response.builder().status("success").message("Movie deleted successfully").build());
 	}
