5185af3244438fbbcd9fc2c239c040fbfb309edf
Prithwish Samanta <wprith@gmail.com>
2023-05-13 22:52:14 +0530

    implement open api doc for book-ticket-service

--- Backend/ticket-booking-service/src/main/java/com/cts/ticketbookingservice/controller/TicketBookingController.java
+++ Backend/ticket-booking-service/src/main/java/com/cts/ticketbookingservice/controller/TicketBookingController.java
@@ -19,14 +19,31 @@ import com.cts.ticketbookingservice.dto.BookTicketRequest;
 import com.cts.ticketbookingservice.dto.Response;
 import com.cts.ticketbookingservice.service.TicketBookingService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Ticket Booking", description = "ticket booking API")
 @RestController
 public class TicketBookingController {
 	@Autowired
 	private TicketBookingService bookingService;
 
+	@Operation(summary = "Book ticket", description = "Book ticket for an upcoming show")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Ticket booking successful", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "404", description = "Movie not found", content = @Content(schema = @Schema(implementation = Response.class))),
+			@ApiResponse(responseCode = "401", description = "Unauthorize. Only a customer can perform this operation", content = @Content(schema = @Schema(implementation = Response.class))) })
 	@PostMapping("/book")
-	public ResponseEntity<Response> bookTicket(@RequestHeader(name = "Authorization") String token,
-			@RequestBody @Valid BookTicketRequest request, BindingResult bindingResult) {
+	public ResponseEntity<Response> bookTicket(
+			@Parameter(description = "JWT auth token of the application user", required = true) @RequestHeader(name = "Authorization") String token,
+			@Parameter(description = "Book ticket request including show id and no of seats", required = true) @RequestBody @Valid BookTicketRequest request,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return errorResponse(bindingResult.getAllErrors());
 		}
